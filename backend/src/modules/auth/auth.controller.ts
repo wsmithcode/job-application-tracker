3271d0a08db0b039a4cbb9c67fdf391b30e03e1c
wsmithcode <wsmith.code@gmail.com>
2025-10-12 18:53:22 +0200

    feat: update auth controller

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post  } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { SignupDto } from './dto/signup.dto';
-import { UsersService } from '@modules/users/users.service';
 
 @Controller('auth')
 export class AuthController {
